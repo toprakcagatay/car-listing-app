b1aff15ab4886311025cbdcb2add9ebc6dca21c1
Cagatay <toprakcagatay@gmail.com>
2024-02-06 01:57:18 +0300

    routing fix

--- src/main.js
+++ src/main.js
@@ -14,8 +14,7 @@ const routes = [
 ]
 const router = createRouter({
   // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-  //history: createWebHashHistory(),
-  history: createMemoryHistory(),
+  history: createWebHashHistory(),
   routes, // short for `routes: routes`
 })
 
